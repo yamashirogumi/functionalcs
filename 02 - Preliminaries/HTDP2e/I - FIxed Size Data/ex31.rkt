2f093df9aa9681029c3acf50093afd5111a9d560
yamashirogumi <56829411+yamashirogumi@users.noreply.github.com>
2019-11-19 11:22:34 -0300

    ex31

---
+++ 02 - Preliminaries/HTDP2e/I - FIxed Size Data/ex31.rkt
@@ -0,0 +1,33 @@
+(require 2htdp/batch-io)
+
+(define (main in-fst in-lst in-signature out)
+  (write-file out
+              (letter (read-file in-fst)
+                      (read-file in-lst)
+                      (read-file in-signature))))
+
+(define (letter in-fst in-lst in-signature)
+  (string-append
+   (opening in-fst)
+  "\n\n"
+  (body in-fst in-lst)
+  "\n\n"
+  (closing in-signature)))
+
+(define (opening in-fst)
+  (string-append " Dear " in-fst ", "))
+
+(define (body in-fst in-lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " in-lst " have won our lottery. So, " "\n"
+   in-fst ", " "hurry and pick up your prize."))
+
+(define (closing in-signature)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+  in-signature
+   "\n"))
+
+(main "nombre" "apellido" "firma" "carta.txt")
